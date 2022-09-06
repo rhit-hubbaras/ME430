7b680331a828908a878cc340350688a2c00d8cd2
rhit-hubbaras <hubbaras@rose-hulman.edu>
2022-09-06 13:09:22 -0400

    lab1partA

---
+++ Lab1PartA/Lab1PartA.ino
@@ -0,0 +1,27 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2,INPUT_PULLUP);
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(4, !digitalRead(2));
+  digitalWrite(5, !digitalRead(3));
+  analogWrite(6, analogRead(0)/4);
+  
+  digitalWrite(12, !digitalRead(2));
+  digitalWrite(13, !digitalRead(3));
+  analogWrite(11, analogRead(0)/4);
+  
+  delay(10);
+}
